2f58e8aa2c12eb4228e57acef20ef556ab1751ab
sriky <srikanth.kyatham@kaltiot.com>
2021-10-18 06:00:11 +0300

    Adding hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,12 +101,6 @@ export default function Home() {
                       </a>
                     ))}
                   </div>
-                  <a
-                    href="#"
-                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                  >
-                    Log in
-                  </a>
                 </div>
               </Popover.Panel>
             </Transition>
@@ -115,9 +109,9 @@ export default function Home() {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Data to enrich your</span>{" "}
+                <span className="block xl:inline">A place to find your</span>{" "}
                 <span className="block text-indigo-600 xl:inline">
-                  online business
+                  pharma needs online
                 </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -150,7 +144,7 @@ export default function Home() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          src="/images/logo.webp"
           alt=""
         />
       </div>
