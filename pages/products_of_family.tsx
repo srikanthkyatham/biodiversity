2508581073a4b2ef152730fc5c3b2a7c27c3f6aa
srikanthkyatham <srikanth.1.kyatham@gmail.com>
2022-06-08 12:47:04 +0300

    Updating project id and products of families

--- pages/products_of_family.tsx
+++ pages/products_of_family.tsx
@@ -6,19 +6,21 @@ interface Props {
   products: Array<ProductType>;
 }
 
-const Products = ({ products }: Props): JSX.Element => {
-  console.log({ products });
+const ProductsOfFamily = ({ products }: Props): JSX.Element => {
+  //console.log({ products });
   return <div />;
 };
 
-const query = groq`*[_type == "product"]{
-  title,
+const query = groq`*[_type == "product" && families[]->title match "Dexamethasone"]{
+  title
 }`;
 
 export async function getStaticProps(context: any) {
   // It's important to default the slug so that it doesn't return "undefined"
   const products = await client.fetch(query);
-  console.log({ products });
+  //console.log({ products });
+  console.log("families");
+  console.dir(products);
   return {
     props: {
       products,
@@ -26,4 +28,4 @@ export async function getStaticProps(context: any) {
   };
 }
 
-export default Products;
+export default ProductsOfFamily;
