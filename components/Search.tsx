72064fefb6289e059e6beb65d0bc1c441dc46561
srikanthkyatham <srikanth.1.kyatham@gmail.com>
2022-09-27 18:47:25 +0300

    Searching based on casNo

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-key */
+import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useState } from "react";
-import { filterSimilarProducts } from '../utils/search'
+import { fetchProducts, productsNameFilter } from "../utils/productsReducer";
+import { filterSimilarProducts } from "../utils/search";
 type ChangeEvent = (e: React.ChangeEvent<HTMLInputElement>) => void;
-import { useRouter } from 'next/router'
-import { fetchProducts, productsNameFilter } from '../utils/productsReducer'
 
 const debounce = (func: any, delay: number) => {
   let setTimoutInstance: any;
@@ -15,29 +15,32 @@ const debounce = (func: any, delay: number) => {
 };
 
 interface Search {
-  products: any
+  products: any;
 }
 
 const Search: React.FC = () => {
-
-  const router = useRouter()
-  const [products,setProducts] = useState<any[]>([])
+  const router = useRouter();
+  const [products, setProducts] = useState<any[]>([]);
   const [search, setSearch] = useState<string>("");
-  const [availableProducts,setAvailableProducts] = useState<any[]>([])
+  const [availableProducts, setAvailableProducts] = useState<any[]>([]);
 
-  const api = async (val: string) => {    
-    const response = await filterSimilarProducts(val.toLowerCase(), products, true).slice(0,10)
-    setAvailableProducts(response)
+  const api = async (val: string) => {
+    const response = await filterSimilarProducts(
+      val.toLowerCase(),
+      products,
+      false
+    ).slice(0, 10);
+    setAvailableProducts(response);
   };
 
   const getProducts = async () => {
-    const response = await fetchProducts()
-    setProducts(response)
-  }
+    const response = await fetchProducts();
+    setProducts(response);
+  };
 
   useEffect(() => {
-    getProducts()
-  }, [])
+    getProducts();
+  }, []);
 
   const func: any = useCallback(
     debounce((string: string) => api(string), 1000),
@@ -53,43 +56,53 @@ const Search: React.FC = () => {
     setSearch(value);
   };
 
-  const redirectToProducts = ( title: string) => {
-    setSearch('')
-    setAvailableProducts([])
-    router.push(`/product/${productsNameFilter(title)}`)
-  }
+  const redirectToProducts = (title: string) => {
+    setSearch("");
+    setAvailableProducts([]);
+    router.push(`/product/${productsNameFilter(title)}`);
+  };
 
   return (
     <div className="xl:w-96">
       <div className="input-group relative flex flex-wrap items-stretch w-full">
         <div className="input-group relative flex flex-wrap items-stretch w-full">
-          <input 
+          <input
             value={search}
-            onChange={changeHandler} 
+            onChange={changeHandler}
             className="form-control relative flex-auto min-w-0 block px-3 pr-10 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border 
-              border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none md:pr-3" 
+              border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none md:pr-3"
             placeholder="Enter CAS No., Catalog No. Or Product Name"
             aria-label="Search"
-            aria-describedby="button-addon2" 
+            aria-describedby="button-addon2"
           />
-          <button style={{ position: 'absolute', right: 10, top: 8 }} onClick={() => {
-            setSearch('')
-            setAvailableProducts([])
-          }
-          }>X</button>
+          <button
+            style={{ position: "absolute", right: 10, top: 8 }}
+            onClick={() => {
+              setSearch("");
+              setAvailableProducts([]);
+            }}
+          >
+            X
+          </button>
         </div>
-        
-        {
-          availableProducts.length > 0 && 
-          <ul className="absolute bg-white pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none text-xl py-5 border border-stone-500 no-scrollbar overflow-scroll w-full" 
-          style={{ top: 37, maxHeight: 300, zIndex: 10000 }}>
-            {
-              availableProducts.map(item => <li className='px-1 py-2 border-b-2' onClick={() => redirectToProducts(item.title)}>{item.title}</li> )
-            }
+
+        {availableProducts.length > 0 && (
+          <ul
+            className="absolute bg-white pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none text-xl py-5 border border-stone-500 no-scrollbar overflow-scroll w-full"
+            style={{ top: 37, maxHeight: 300, zIndex: 10000 }}
+          >
+            {availableProducts.map((item) => (
+              <li
+                className="px-1 py-2 border-b-2"
+                onClick={() => redirectToProducts(item.title)}
+              >
+                {item.title}
+              </li>
+            ))}
           </ul>
-        }
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
--- components/Watermark/index.js
+++ components/Watermark/index.js
@@ -1,20 +1,31 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
-import { imageFormatter } from '../../utils/imageFormatter'
-import Config from '../../app.json'
-import { Watermark } from '@hirohe/react-watermark';
+import { imageFormatter } from "../../utils/imageFormatter";
+import Config from "../../app.json";
+import { Watermark } from "@hirohe/react-watermark";
 
 const WatermarkComponent = ({ product }) => {
+  const [screenWidth, setScreenWidth] = useState(window.screen.width || 700);
 
-  const [screenWidth,setScreenWidth] = useState(window.screen.width || 700)
+  //console.log(imageFormatter(screenWidth,product))
 
-  console.log(imageFormatter(screenWidth,product))
+  return (
+    <>
+      <Watermark
+        text={Config.watermark_text}
+        textColor="#808080"
+        textSize={35}
+        rotate={0}
+        gutter={150}
+        wrapperStyle={{ minHeight: 150 }}
+      >
+        <img
+          src={product ? imageFormatter(screenWidth, product) : "/logo.png"}
+          alt={product.title}
+        />
+      </Watermark>
+    </>
+  );
+};
 
-  return <>
-    <Watermark text={Config.watermark_text} textColor='#808080' textSize={35} rotate={0} gutter={150} wrapperStyle={{ minHeight: 150 }}>
-      <img src={product ? imageFormatter(screenWidth,product) : '/logo.png'} alt={product.title} />
-    </Watermark>
-  </>
-}
-
-export default WatermarkComponent
+export default WatermarkComponent;
