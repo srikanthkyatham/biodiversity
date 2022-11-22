ea2ec003ff83d9f3165886d4bd1915a6ead33770
srikanthkyatham <srikanth.1.kyatham@gmail.com>
2022-11-22 11:27:44 +0530

    Indenting and log change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,16 @@
-import dynamic from 'next/dynamic'
-import Description from '../components/Description'
-import Footer from '../components/Footer'
-import HomePageProducts from '../components/HomePageProducts'
-import UpcomingProducts from '../components/UpcomingProducts'
-import ContactUs from '../components/ContactUs'
-import Accreditation from '../components/Accreditation'
+import dynamic from "next/dynamic";
+import Accreditation from "../components/Accreditation";
+import ContactUs from "../components/ContactUs";
+import Description from "../components/Description";
+import Footer from "../components/Footer";
+import HomePageProducts from "../components/HomePageProducts";
+import UpcomingProducts from "../components/UpcomingProducts";
 
-const MainImage = dynamic(
-  () => import('../components/MainImage'),
-  { ssr: false }
-)
+const MainImage = dynamic(() => import("../components/MainImage"), {
+  ssr: false,
+});
 
 export default function Home() {
-
   return (
     <div>
       <main>
@@ -20,7 +18,10 @@ export default function Home() {
         <Description />
         <HomePageProducts />
         <UpcomingProducts />
-        <ContactUs displayEnquiryForm={false} onConfirmation={() => console.log('')} />
+        <ContactUs
+          displayEnquiryForm={false}
+          onConfirmation={() => console.log("confirmation clicked")}
+        />
         <Accreditation />
         <Footer />
       </main>
