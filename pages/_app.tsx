2e82e095053dd585c7468b4830f846d3b7ca53a9
srikanthkyatham <srikanth.1.kyatham@gmail.com>
2023-01-03 21:47:27 +0200

    Adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react'
+import { Analytics } from "@vercel/analytics/react";
+import { useEffect, useState } from "react";
+import EnquiryForm from "../components/EnquiryForm";
+import Header from "../components/Header";
 import Layout from "../components/Layout";
+import NavigationHeader from "../components/NavigationHeader";
 import "../styles/globals.css";
-import Header from '../components/Header'
-import NavigationHeader from '../components/NavigationHeader'
-import { whatsappChat } from '../utils/whatsappChat'
-import EnquiryForm from '../components/EnquiryForm'
+import { whatsappChat } from "../utils/whatsappChat";
 
 type Props = {
   Component: any;
@@ -12,40 +13,54 @@ type Props = {
 };
 
 function MyApp({ Component, pageProps }: Props) {
-
-  const [whatsAppDisable,setWhatsAppDisable] = useState(false)
+  const [whatsAppDisable, setWhatsAppDisable] = useState(false);
 
   const windowScroll = () => {
-    if(typeof window !== "undefined" && window) {
-      window.scrollTo(0,0)
+    if (typeof window !== "undefined" && window) {
+      window.scrollTo(0, 0);
     }
-  }
+  };
 
   const whatsAppBrowserCheck = () => {
-    if(typeof window !== "undefined" && window && navigator) {
-      if(/iPhone|iPad|iPod/i.test(window.navigator.userAgent)) {
-        setWhatsAppDisable(true)
+    if (typeof window !== "undefined" && window && navigator) {
+      if (/iPhone|iPad|iPod/i.test(window.navigator.userAgent)) {
+        setWhatsAppDisable(true);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    whatsAppBrowserCheck()
-  },[])
+    whatsAppBrowserCheck();
+  }, []);
 
   return (
-    <Layout>
-      <Header />
-      <NavigationHeader />
-      <Component {...pageProps} />
-      {!whatsAppDisable && <a onClick={() => whatsappChat()} className="float" target="_blank" rel="noreferrer">
-        <i className="fa fa-whatsapp my-float cursor-pointer" />
-      </a>}
-      <a onClick={windowScroll} className="scroll-top-float" target="_blank" rel="noreferrer">
-        <i className="fa fa-solid fa-arrow-up my-float cursor-pointer" />
-      </a>
-      <EnquiryForm />
-    </Layout>
+    <>
+      <Layout>
+        <Header />
+        <NavigationHeader />
+        <Component {...pageProps} />
+        {!whatsAppDisable && (
+          <a
+            onClick={() => whatsappChat()}
+            className="float"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-whatsapp my-float cursor-pointer" />
+          </a>
+        )}
+        <a
+          onClick={windowScroll}
+          className="scroll-top-float"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa fa-solid fa-arrow-up my-float cursor-pointer" />
+        </a>
+        <EnquiryForm />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
 
