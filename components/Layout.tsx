1ff6b9dca2952d05b52b97292f6d2bc4e85f7d5f
srikanthkyatham <srikanth.1.kyatham@gmail.com>
2023-05-10 16:33:40 +0300

    Fixing linting errors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import { description } from "../lib/constants";
-import { useRouter } from 'next/router';
 
 type Props = {
   children: ReactNode;
@@ -10,33 +9,61 @@ type Props = {
 const title = "Bio diversity sciences";
 
 export default function Layout({ children }: Props) {
-    const router = useRouter();
+  const router = useRouter();
 
   return (
     <>
       <Head>
-        { (router.pathname === '/' || router.pathname === '') ?
-               
-              <>
-                   <title>Delivering Diversity in Biosciences Research | Diversity BioSciences</title>
-                   <meta property="og:title" content="Delivering Diversity in Biosciences Research | Diversity BioSciences" key="title" />
-                   <meta name="description" content="Diversity BioSciences is a research org for the Custom synthesis of new products, committed to providing services/solutions to the Pharmaceutical & Biotech industries." />
-                   <meta property="og:description " content="Diversity BioSciences is a research org for the Custom synthesis of new products, committed to providing services/solutions to the Pharmaceutical & Biotech industries." />
-                   <meta name="keywords" content="Diversity BioSciences, biosciences, bioscience research, Pharmaceutical & Biotech industries, Biotech industries, Bio Sciences research organization" />
-              </>
-         :
-              ((router.pathname === '/families' || router.pathname === 'families') ?
-                   <>
-                        <title>Custom Synthesis of Intermediates and API's | Diversity BioSciences</title>
-                        <meta property="og:title" content="Custom Synthesis of Intermediates and API's | Diversity BioSciences" key="title" />
-                        <meta name="description" content="We developed deep expertise in Custom Synthesis, Intermediates & APl's development and have continuously invested in its people resources and infrastructure." />
-                        <meta property="og:description " content="We developed deep expertise in Custom Synthesis, Intermediates & APl's development and have continuously invested in its people resources and infrastructure." />
-                        <meta name="keywords" content="custom synthesis, intermediates, API's, pharmaceuticals, chemical synthesis, organic chemistry, process development" />
-                   </>
-              :
-                   null
-              )
-         }
+        {router.pathname === "/" || router.pathname === "" ? (
+          <>
+            <title>
+              Delivering Diversity in Biosciences Research | Diversity
+              BioSciences
+            </title>
+            <meta
+              property="og:title"
+              content="Delivering Diversity in Biosciences Research | Diversity BioSciences"
+              key="title"
+            />
+            <meta
+              name="description"
+              content="Diversity BioSciences is a research org for the Custom synthesis of new products, committed to providing services/solutions to the Pharmaceutical & Biotech industries."
+            />
+            <meta
+              property="og:description "
+              content="Diversity BioSciences is a research org for the Custom synthesis of new products, committed to providing services/solutions to the Pharmaceutical & Biotech industries."
+            />
+            <meta
+              name="keywords"
+              content="Diversity BioSciences, biosciences, bioscience research, Pharmaceutical & Biotech industries, Biotech industries, Bio Sciences research organization"
+            />
+          </>
+        ) : router.pathname === "/families" ||
+          router.pathname === "families" ? (
+          <>
+            <title>
+              Custom Synthesis of Intermediates and API@aposs | Diversity
+              BioSciences
+            </title>
+            <meta
+              property="og:title"
+              content="Custom Synthesis of Intermediates and API's | Diversity BioSciences"
+              key="title"
+            />
+            <meta
+              name="description"
+              content="We developed deep expertise in Custom Synthesis, Intermediates & APl's development and have continuously invested in its people resources and infrastructure."
+            />
+            <meta
+              property="og:description "
+              content="We developed deep expertise in Custom Synthesis, Intermediates & APl's development and have continuously invested in its people resources and infrastructure."
+            />
+            <meta
+              name="keywords"
+              content="custom synthesis, intermediates, API's, pharmaceuticals, chemical synthesis, organic chemistry, process development"
+            />
+          </>
+        ) : null}
       </Head>
       <div>{children}</div>
     </>
